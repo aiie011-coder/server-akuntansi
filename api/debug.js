d9e031167a48a2ca2f191271eec459d9873f11ea
aiie011-coder <arieaiie011@gmail.com>
2026-04-19 18:06:36 +0800

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,7 +1,7 @@
 // File: api/debug.js
 // HAPUS FILE INI setelah selesai debug!
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -23,4 +23,4 @@ export default async function handler(req, res) {
     match: secret === incoming,
     method: req.method
   });
-}
+};
